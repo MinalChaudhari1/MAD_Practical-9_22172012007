bec578f04fe660fc61a72dad176f0109e3d845d4
student <student@gnu.ac.in>
2023-10-18 08:55:23 +0530

    hey

--- app/src/main/java/com/example/mad_practical_9_22172012007/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_22172012007/SplashActivity.kt
@@ -1,11 +1,36 @@
 package com.example.mad_practical_9_22172012007
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(),AnimationListener{
+    lateinit var  TwinAnimation:Animation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+
     }
 }
